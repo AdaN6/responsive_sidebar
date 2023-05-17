1717635ccbab6b0e9309373d0bdae0d92e531810
AdaN6 <a_disney_da@hotmail.com>
2023-05-17 19:17:40 +0200

    moved routes to router folder

--- src/main.js
+++ src/main.js
@@ -1,21 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { createRouter, createWebHistory} from 'vue-router'
-import Home from './views/Home.vue'
-import About from './views/About.vue'
+import router from './router'
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            component: Home
-        },
-        {
-            path: '/about',
-            component: About
-        }
-    ]
-})
 
 createApp(App).use(router).mount('#app')
