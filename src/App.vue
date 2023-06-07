500aea8ba58fb36679349c82ecd5e6b6b656ee50
AdaN6 <a_disney_da@hotmail.com>
2023-06-07 15:37:25 +0200

    styled basic sidebar, added vue logo

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,6 @@ import Sidebar from "./components/Sidebar.vue"
 <style lang="scss">
 :root {
 	--primary: #4ade80;
-	--primary-alt: #22c55e;
 	--grey: #64748b;
 	--dark: #1e293b;
 	--dark-alt: #334155;
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,8 @@
 <template>
     <aside>
-        Sidebar
+        <div class="log">
+            <img src="../assets/vue.svg" alt="Vue">
+        </div>
     </aside>
   
 </template>
@@ -12,5 +14,32 @@
 </script>
 
 <style lang="scss" scoped>
+aside{
+    display: flex;
+    flex-direction: column;
+    width: calc(2rem + 32px);
+    overflow: hidden;
+    min-height: 100vh;
+    padding: 1rem;
+
+
+    background-color: var(--dark);
+    color: var(--light);
+
+    transition: 0.2s ease-out;
+
+    .logo {
+        margin-bottom: 1rem;
+
+        img{
+            width: 2rem;
+        }
+    }
+
+    @media (max-width: 768px) {
+        position: fixed;
+        z-index: 99;
+    }
+}
 
 </style>
